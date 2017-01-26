93b0ec577dd2a72b6822f1fec7dbcd10dc6307f0
sreeanne <sreedhar.anne@gmail.com>
2017-01-26 17:37:42 -0500

    1/26
    1/26

--- workspace/JSON/src/jtest/JsonSimpleWriteExample.java
+++ workspace/JSON/src/jtest/JsonSimpleWriteExample.java
@@ -1,5 +1,6 @@
 package jtest;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,27 +12,36 @@ public class JsonSimpleWriteExample {
 	@SuppressWarnings("unchecked") 
 	public static void main(String[] args) {
 		
-		JSONObject obj = new JSONObject();
-        obj.put("name", "mkyong.com");
-        obj.put("age", new Integer(100));
-
-        JSONArray list = new JSONArray();
-        list.add("msg 1");
-        list.add("msg 2");
-        list.add("msg 3");
-
-        obj.put("messages", list);
-
-        try (FileWriter file = new FileWriter("test.json")) {
-
-            file.write(obj.toJSONString());
-            file.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+		JSONObject countryObj = new JSONObject();  
+        countryObj.put("Name", "US");  
+        countryObj.put("Population", new Integer(30000000));  
+  
+        JSONArray listOfStates = new JSONArray();  
+        listOfStates.add("Arizone");  
+        listOfStates.add("Florida");  
+        listOfStates.add("Ohio");  
+  
+        countryObj.put("States", listOfStates);  
+  
+        try {  
+              
+            // Writing to a file  
+            File file=new File("test.json");  
+            file.createNewFile();
+            
+            FileWriter fileWriter = new FileWriter(file);  
+            
+            System.out.println("Writing JSON object to file");  
+            System.out.println("-----------------------");  
+            System.out.print(countryObj);  
+  
+            fileWriter.write(countryObj.toJSONString());  
+            fileWriter.flush();  
+            fileWriter.close();  
+  
+        } catch (IOException e) {  
+            e.printStackTrace();  
         }
-
-        System.out.print(obj);
 
 	}
 
